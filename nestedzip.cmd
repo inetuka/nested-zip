12532dd5525e627e1275085e860c48056632ea52
Udo Kaune <github@inet-hamburg.de>
2015-08-25 14:55:34 +0200

    Update nestedzip.cmd

--- nestedzip.cmd
+++ nestedzip.cmd
@@ -1,69 +1,130 @@
 @echo off
+rem	--------------------------------------------------------------------------
+rem	name	nestedzip.cmd
+rem	usage	mdaemon nested zip mitigation
+rem	owner	iNet, uka
+rem	date	150824
+rem	depend	%comspec%=cmd.exe
+rem			grep.exe
+rem			unzip.exe
+rem			uud64win.exe
+rem
+rem         This file is public domain and provided as-is.
+rem			There are no guarantees. Use at your own risk.
+rem
+rem	--------------------------------------------------------------------------
 
-set nzPATH="C:\MDaemon\Public Folders\Ziptest.IMAP"
+SETLOCAL
 
-echo start %date% %time%>> %nzPATH%\nestedzip.log
+rem set your MD base install path
+rem
+set "MDPATH=D:\Wan\Mdaemon"
 
-if not exist %nzPATH%\*.msg goto :eof
+rem Assumed you have a special folder for your system logs set it here,
+rem otherwise logging is to %TEMP% folder.
+rem
+if "%LOGS%" EQU "" set "LOGS=%TEMP%"
 
-for /f "tokens=*" %%i in ('dir /l /b /a:-d %nzPATH%\*.msg') do ren %nzPATH%\%%i *.msz
+rem Path to folder with messages to be analysed, this is where messages with
+rem zip attachments got moved to by MD. You need a MD rule = "if message has
+rem file like *.zip move to this folder and stop processing".
+rem You have to create these folders by hand or from within MD.
+rem CAVE: The folder must be accessible by MD content filter move rules.
+rem I chose \Public Folders\Ziptest
+rem                                \Nested
+rem                                \Ok
+rem 
+set "nzPATH=%MDPATH%\Public Folders\Ziptest.IMAP"
 
-rem für alle existierenden .msz
-for /f "tokens=*" %%i in ('dir /l /b /a:-d %nzPATH%\*.msz') do call :decompose %%i
+set "nzNEST=%nzPATH%\Nested.IMAP"
+set "nzISOK=%nzPATH%\Ok.IMAP"
+
+rem Leave this place if there is no work to do
+rem
+if not exist "%nzPATH%\*.msg" goto :eof
+
+rem Do some logging
+rem
+echo start %0 %date% %time%>> "%LOGS%\nestedzip.log"
+
+rem Change extensions of files we want to process to be sure MD is done
+rem with the move.
+rem
+for /f "tokens=*" %%i in ('dir /l /b /a:-d "%nzPATH%\*.msg"') do ren "%nzPATH%\%%i" *.msz
+
+rem Decompose messages into components, body text, decoded attachments...
+rem
+for /f "tokens=*" %%i in ('dir /l /b /a:-d "%nzPATH%\*.msz"') do call :decompose %%i
+
+rem Analyse the now decoded mail components
+rem
+for /f "tokens=*" %%i in ('dir /l /b /a:d "%nzPATH%\pd*"') do call :getzip %%i
+
+rem If there are still *.msz in the %nzPATH% after the content has been analysed
+rem these are the "good" ones. Move them to the %nzISOK% folder and rename back
+rem to .msg.
+rem
+if exist "%nzPATH%\*.msz" (
+
+	move "%nzPATH%\*.msz" "%nzISOK%\."
+	for /f "tokens=*" %%i in ('dir /l /b /a:-d "%nzISOK%\*.msz"') do ren "%nzISOK%\%%i" *.msg
 	
-rem für alle decomposed mails
-for /f "tokens=*" %%i in ('dir /l /b /a:d %nzPATH%\pd*') do call :getzip %%i
+	)
 
-move %nzPATH%\*.msz %nzPATH%\Ok.IMAP\.
+rem Feed back the messages to the local mail queue of MD.
+rem CAVE: If you have changed the MD local queue path from default you have to do it here too.
+rem 
+if exist "%nzISOK%\*.msg" move "%nzISOK%\*.msg" "%MDPATH%\Queues\Local\."
 
-for /f "tokens=*" %%i in ('dir /l /b /a:-d %nzPATH%\Ok.IMAP\*.msz') do ren %nzPATH%\Ok.IMAP\%%i *.msg
-
-move %nzPATH%\Ok.IMAP\*.msg C:\MDaemon\Queues\Local\.
-
-echo stop %date% %time%>> %nzPATH%\nestedzip.log
+rem do some more logging
+rem
+echo stop %0 %date% %time%>> "%LOGS%\nestedzip.log"
 
 goto :eof
 
 :decompose
 
-	set dzN=%~n1
-	set dzPATH=%nzPATH:"=%
+	set "nzN=%~n1"
 	
-	rem verzeichnis anlegen aus namen
-	mkdir "%dzPATH%\%dzN%"
+	mkdir "%nzPATH%\%nzN%"
 
-	rem dorthin decomposen
-	uud64win /extract /outdir="%dzPATH%\%dzN%" "%dzPATH%\%1"
+	uud64win /extract /outdir="%nzPATH%\%nzN%" "%nzPATH%\%1"
 	
 	goto :eof
 
 :getzip
 	
-	set IS_NESTED=N
+	set "IS_NESTED=N"
 	
-	for /f "tokens=*" %%j  in ('dir /l /b /s /a:-d %nzPATH%\%1\*.zip') do call :listzip "%%j"
+	for /f "tokens=*" %%j  in ('dir /l /b /s /a:-d "%nzPATH%\%1\*.zip"') do call :listzip "%%j"
 
 	if "%IS_NESTED%" EQU "Y" (
 	
-		move %nzPATH%\%1.msz %nzPATH%\Nested.IMAP\.
-		ren  %nzPATH%\Nested.IMAP\%1.msz %1.msg
+		move "%nzPATH%\%1.msz" "%nzNEST%\."
+		ren  "%nzNEST%\%1.msz" %1.msg
 		
 	)
 	
-	rmdir /s /q %nzPATH%\%1
+	rem Remove temporary folder for decoded message
+	rem
+	rmdir /s /q "%nzPATH%\%1"
 	
 	goto :eof
 
 :listzip
 
-	set dzN=%~n1
-	set dzPATH=%~dp1
+	set "dzN=%~n1"
+	set "dzPATH=%~dp1"
 	
+	rem CAVE: unzip must not report back any more info than files contained in the zip.
+	rem If it lists the zip name itself grep will always succeed. This is not what we want.
+	rem Check that -qql (minus cue cue ell) is understood correctly
+	rem
 	unzip -qql "%dzPATH%%dzN%.zip" > "%dzPATH%%dzN%.zip.lst"
 
 	grep ".zip" "%dzPATH%%dzN%.zip.lst"
-	if "%errorlevel%" EQU "0" set IS_NESTED=Y
+	if "%errorlevel%" EQU "0" set "IS_NESTED=Y"
 	
 	goto :eof
 
-:eof
+rem eof nestedzip.cmd 
